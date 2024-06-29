f2c5f432cf5639baaed34242357ab1615a6f732a
Marcos Assuncao <assuncao@acm.org>
2024-06-29 16:16:00 -0400

    Partial implementation of the fog simulation

--- offloading_gym/simulation/fog/resources.py
+++ offloading_gym/simulation/fog/resources.py
@@ -8,13 +8,63 @@ scheduling in a fog environment.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, List, Dict
+from typing import TYPE_CHECKING, List, Dict, Union, Optional
 
 import math
+import numpy as np
+import itertools
+import simpy
 
 from simpy.resources.resource import Resource, Request, Release
 from simpy.core import BoundClass, Environment, SimTime
 from simpy.exceptions import SimPyException
+from gymnasium.utils import seeding
+
+from .typing import (
+    Coordinate,
+    RectGeographicalArea,
+    Interval,
+    ResourceConfig,
+    ResourceGroupConfig,
+    ComputingConfig,
+    NetworkConfig,
+)
+
+
+# The following GPS coordinates roughly encompass the entire
+# city of Montreal, forming a rough rectangular boundary around it.
+MONTREAL_AREA = RectGeographicalArea(
+    northeast=Coordinate(lat=45.7057, long=73.4746),
+    northwest=Coordinate(lat=45.7057, long=73.9434),
+    southeast=Coordinate(lat=45.3831, long=73.4746),
+    southwest=Coordinate(lat=45.3831, long=73.9434),
+)
+
+DEFAULT_COMP_CONFIG = ComputingConfig(
+    iot=ResourceGroupConfig(
+        num_resources=1,
+        resource_config=ResourceConfig(
+            cpu_cores=[1], cpu_core_speed=[1.0], memory=[1.0]
+        ),
+        deployment_area=MONTREAL_AREA,
+    ),
+    edge=ResourceGroupConfig(
+        num_resources=36,
+        resource_config=ResourceConfig(
+            cpu_cores=[4], cpu_core_speed=[1.5, 1.8, 2.0], memory=[1.0, 2.0, 4.0]
+        ),
+        network_config=NetworkConfig(bandwidth=Interval(min=10, max=12)),
+        deployment_area=MONTREAL_AREA,
+    ),
+    cloud=ResourceGroupConfig(
+        num_resources=20,
+        resource_config=ResourceConfig(
+            cpu_cores=[8], cpu_core_speed=[2.0, 2.6, 3.0], memory=[16.0, 24.0, 32.0]
+        ),
+        network_config=NetworkConfig(bandwidth=Interval(min=4, max=8)),
+        deployment_area=MONTREAL_AREA,
+    ),
+)
 
 
 class ComputeRequest(Request):
@@ -29,6 +79,7 @@ class ComputeRequest(Request):
     a :keyword:`with` statement.
 
     """
+
     cpu_cores: int
     memory: float
     time: SimTime
@@ -41,9 +92,9 @@ class ComputeRequest(Request):
 
     def __str__(self):
         return (
-            f'ComputeRequest<cpu_cores={self.cpu_cores}, '
-            f'memory={self.memory}, '
-            f'time={self.time}>'
+            f"ComputeRequest<cpu_cores={self.cpu_cores}, "
+            f"memory={self.memory}, "
+            f"time={self.time}>"
         )
 
 
@@ -121,13 +172,19 @@ class ComputeResource(Resource):
         release = BoundClass(Release)
 
     def _do_put(self, event: ComputeRequest) -> None:
-        if event.cpu_cores <= self._available_cpu_cores and event.memory <= self._available_memory:
+        if (
+            event.cpu_cores <= self._available_cpu_cores
+            and event.memory <= self._available_memory
+        ):
             self._available_memory -= event.memory
             self._available_cpu_cores -= event.cpu_cores
             self.users.append(event)
             event.usage_since = self._env.now
             event.succeed()
-        elif event.cpu_cores > self.number_of_cores or event.memory > self._memory_capacity:
+        elif (
+            event.cpu_cores > self.number_of_cores
+            or event.memory > self._memory_capacity
+        ):
             event.fail(SimPyException("Request exceeded maximum capacity"))
 
     def _do_get(self, event: Release) -> None:
@@ -142,12 +199,12 @@ class ComputeResource(Resource):
 
     def __str__(self):
         return (
-            f'ComputeResource<n_cpu_cores={self.capacity}, '
-            f'cpu_core_speed={self._cpu_core_speed}, '
-            f'available_cpu_cores={self._available_cpu_cores}, '
-            f'memory_capacity={self._memory_capacity}, '
-            f'available_memory={self._available_memory}, '
-            f'queue=[{[str(r) for r in self.queue]}]>'
+            f"ComputeResource<n_cpu_cores={self.capacity}, "
+            f"cpu_core_speed={self._cpu_core_speed}, "
+            f"available_cpu_cores={self._available_cpu_cores}, "
+            f"memory_capacity={self._memory_capacity}, "
+            f"available_memory={self._available_memory}, "
+            f"queue=[{[str(r) for r in self.queue]}]>"
         )
 
 
@@ -164,15 +221,15 @@ class GeolocationResource(ComputeResource):
 
     def __str__(self):
         return (
-            f'GeolocationResource<resource_id={self.resource_id}, '
-            f'latitude={self.latitude}, '
-            f'longitude={self.longitude}>, '
-            f'n_cpu_cores={self.capacity}, '
-            f'cpu_core_speed={self.cpu_core_speed}, '
-            f'available_cpu_cores={self.available_cpu_cores}, '
-            f'memory_capacity={self.memory_capacity}, '
-            f'available_memory={self.available_memory}, '
-            f'queue=[{[str(r) for r in self.queue]}]>'
+            f"GeolocationResource<resource_id={self.resource_id}, "
+            f"latitude={self.latitude}, "
+            f"longitude={self.longitude}>, "
+            f"n_cpu_cores={self.capacity}, "
+            f"cpu_core_speed={self.cpu_core_speed}, "
+            f"available_cpu_cores={self.available_cpu_cores}, "
+            f"memory_capacity={self.memory_capacity}, "
+            f"available_memory={self.available_memory}, "
+            f"queue=[{[str(r) for r in self.queue]}]>"
         )
 
     @staticmethod
@@ -193,9 +250,151 @@ class GeolocationResource(ComputeResource):
         return math.sqrt((lat1 - lat2) ** 2 + (lon1 - lon2) ** 2)
 
 
-class FogEnvironment:
-    _resources: Dict[int, GeolocationResource]
+class ComputingEnvironment:
+    _iot_devices: List[GeolocationResource]
+    _edge_resources: List[GeolocationResource]
+    _cloud_resources: List[GeolocationResource]
+    _all_resources: Dict[int, GeolocationResource]
+    _simpy_env: simpy.Environment
+
+    # The random number generator is a class variable
+    np_random: Union[np.random.Generator, None] = None
+
+    def __init__(
+        self,
+        simpy_env: simpy.Environment,
+        iot_devices: List[GeolocationResource],
+        edge_resources: List[GeolocationResource],
+        cloud_resources: List[GeolocationResource],
+    ):
+        self._simpy_env = simpy_env
+        self._iot_devices = iot_devices
+        self._edge_resources = edge_resources
+        self._cloud_resources = cloud_resources
+        self._all_resources = {}
+
+        for resource_list in [iot_devices, edge_resources, cloud_resources]:
+            for resource in resource_list:
+                self._all_resources[resource.resource_id] = resource
 
     @staticmethod
-    def build(**config) -> FogEnvironment:
-        return None
+    def build(
+        simpy_env: simpy.Environment,
+        *,
+        seed: Optional[int] = None,
+        config: Optional[ComputingConfig] = DEFAULT_COMP_CONFIG,
+    ) -> ComputingEnvironment:
+
+        # Initialize the RNG only the first time the method is called
+        if ComputingEnvironment.np_random is None:
+            if seed is not None:
+                ComputingEnvironment.np_random, _ = seeding.np_random(seed)
+            else:
+                ComputingEnvironment.np_random, _ = seeding.np_random()
+
+        rand = ComputingEnvironment.np_random
+        resource_ids = itertools.count(start=0)
+        iot_config = config.iot
+
+        # Create the IoT device(s)
+        coordinates = ComputingEnvironment.random_coordinates(
+            np_random=rand,
+            num_locations=iot_config.num_resources,
+            area=iot_config.deployment_area,
+        )
+
+        iot_resources = ComputingEnvironment.create_resources(
+            iot_config, coordinates, resource_ids, rand, simpy_env
+        )
+
+        # Create edge resources
+        edge_config = config.edge
+        coordinates = ComputingEnvironment.grid_coordinates(
+            num_locations=edge_config.num_resources, area=edge_config.deployment_area
+        )
+
+        edge_resources = ComputingEnvironment.create_resources(
+            edge_config, coordinates, resource_ids, rand, simpy_env
+        )
+
+        # Create cloud resources
+        cloud_config = config.cloud
+        coordinates = ComputingEnvironment.dummy_coordinates(
+            num_locations=cloud_config.num_resources, area=cloud_config.deployment_area
+        )
+
+        cloud_resources = ComputingEnvironment.create_resources(
+            cloud_config, coordinates, resource_ids, rand, simpy_env
+        )
+
+        return ComputingEnvironment(
+            simpy_env=simpy_env,
+            iot_devices=iot_resources,
+            edge_resources=edge_resources,
+            cloud_resources=cloud_resources,
+        )
+
+    @staticmethod
+    def grid_coordinates(
+        num_locations: int, area: RectGeographicalArea
+    ) -> List[Coordinate]:
+        locations_per_side = round(np.sqrt(num_locations))
+        lat_values = np.linspace(
+            area.northwest.lat, area.southeast.lat, num=locations_per_side
+        )
+        long_values = np.linspace(
+            area.northwest.long, area.southeast.long, num=locations_per_side
+        )
+        locations = [
+            Coordinate(lat=lat, long=long) for lat in lat_values for long in long_values
+        ]
+
+        return locations[:num_locations]
+
+    @staticmethod
+    def random_coordinates(
+        np_random: np.random.Generator, num_locations: int, area: RectGeographicalArea
+    ) -> List[Coordinate]:
+        lat_values = np_random.uniform(
+            area.northwest.lat, area.southeast.lat, size=num_locations
+        )
+        long_values = np_random.uniform(
+            area.northwest.long, area.southeast.long, size=num_locations
+        )
+        locations = [
+            Coordinate(lat=lat, long=long) for lat, long in zip(lat_values, long_values)
+        ]
+        return locations
+
+    @staticmethod
+    def dummy_coordinates(
+        num_locations: int, area: RectGeographicalArea
+    ) -> List[Coordinate]:
+        return [
+            Coordinate(lat=area.southeast.lat, long=area.southeast.long)
+            for _ in range(num_locations)
+        ]
+
+    @staticmethod
+    def create_resources(
+        config: ResourceGroupConfig,
+        coordinates: List[Coordinate],
+        resource_ids: itertools.count,
+        rand: np.random,
+        simpy_env: Environment,
+    ):
+        return [
+            GeolocationResource(
+                resource_id=next(resource_ids),
+                latitude=coord.lat,
+                longitude=coord.long,
+                env=simpy_env,
+                n_cpu_cores=rand.choice(config.resource_config.cpu_cores),
+                cpu_core_speed=rand.choice(config.resource_config.cpu_core_speed),
+                memory_capacity=rand.choice(config.resource_config.memory),
+            )
+            for coord in coordinates
+        ]
+
+    def resources(self) -> Dict[int, GeolocationResource]:
+        return self._all_resources
---
+++ offloading_gym/simulation/fog/typing.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from typing import List, NamedTuple, Union
+
+
+class Coordinate(NamedTuple):
+    """Namedtuple that represents a geographical coordinate."""
+
+    lat: float
+    long: float
+
+
+class RectGeographicalArea:
+    """
+    Set of geographical coordinates that form a rectangular area on which
+    edge resources are deployed.
+    """
+
+    def __init__(
+        self,
+        *,
+        northeast: Coordinate,
+        northwest: Coordinate,
+        southeast: Coordinate,
+        southwest: Coordinate,
+    ):
+        self._northeast = northeast
+        self._northwest = northwest
+        self._southeast = southeast
+        self._southwest = southwest
+
+        if not self._is_rectangle():
+            raise ValueError("Coordinates do not form a rectangle")
+
+    @property
+    def northeast(self):
+        return self._northeast
+
+    @property
+    def northwest(self):
+        return self._northwest
+
+    @property
+    def southeast(self):
+        return self._southeast
+
+    @property
+    def southwest(self):
+        return self._southwest
+
+    def _is_rectangle(self):
+        # Check to see if the coordinates form a rectangle
+        return (
+            self.northeast.lat == self.northwest.lat
+            and self.southeast.lat == self.southwest.lat
+            and self.northeast.long == self.southeast.long
+            and self.northwest.long == self.southwest.long
+        )
+
+
+class ResourceConfig(NamedTuple):
+    """Data type for computing resource configuration."""
+
+    cpu_cores: List[int]
+    cpu_core_speed: List[float]
+    memory: List[float]
+
+
+class Interval(NamedTuple):
+    """Data type for a value interval."""
+
+    min: float
+    max: float
+
+
+class NetworkConfig(NamedTuple):
+    """Data type for computing network configuration."""
+
+    bandwidth: Interval
+
+
+class ResourceGroupConfig(NamedTuple):
+    """
+    Data type for the configuration of a group of
+    resources (e.g., IoT devices, edge servers, cloud servers)
+    """
+
+    num_resources: int
+    resource_config: ResourceConfig
+    network_config: Union[NetworkConfig, None]
+    deployment_area: Union[RectGeographicalArea, List[Coordinate]]
+
+
+class ComputingConfig(NamedTuple):
+    """Data type for the configuration data of a fog environment."""
+
+    iot: ResourceGroupConfig
+    edge: ResourceGroupConfig
+    cloud: ResourceGroupConfig
