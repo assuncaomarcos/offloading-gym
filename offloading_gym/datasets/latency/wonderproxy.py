2b65a93684125c9518fa11808d8b999c6588948a
Marcos Assuncao <assuncao@acm.org>
2024-06-30 09:57:06 -0400

    Working version of the latency datasets

--- offloading_gym/datasets/latency/wonderproxy.py
+++ offloading_gym/datasets/latency/wonderproxy.py
@@ -6,14 +6,17 @@ Module for downloading and loading the WonderProxy dataset to
 derive minimum and maximum latencies between a source city and
 multiple servers in selected target countries.
 
-Usage:
-    To download the dataset and generate the latency files in the ./data directory:
-    $ python -m offloading_gym.datasets.latency --output_dir=./data
+Usage example:
+    To download the dataset to ./data directory, and compute the latencies
+    from Montreal (id 52) to all locations in Canada and the United States:
+
+    $ python -m offloading_gym.datasets.latency --output_dir=./data --source_id 52 \
+        --destination Canada --destination "United States"
 """
 
 from os import path, makedirs
 
-from typing import AnyStr
+from typing import AnyStr, List
 from collections import namedtuple
 
 import click
@@ -30,6 +33,16 @@ DESTINATION_COUNTRIES = ["United States", "Canada"]
 SOURCE_ID = 52  # The id for Montreal
 
 Datasets = namedtuple("Datasets", ["servers", "pings"])
+
+
+def valid_id(df: pd.DataFrame, location_id: int) -> bool:
+    """Tests whether the provided location id is valid."""
+    return df["id"].isin([location_id]).any()
+
+
+def valid_country(df: pd.DataFrame, country: AnyStr) -> bool:
+    """Tests whether the provided country is valid."""
+    return df["country"].isin([country]).any()
 
 
 def fetch_data(cache_path: AnyStr) -> Datasets:
@@ -55,34 +68,64 @@ def fetch_data(cache_path: AnyStr) -> Datasets:
     )
 
 
-def create_latency_files(datasets: Datasets, output_dir: AnyStr):
+def create_latency_files(
+    datasets: Datasets, output_dir: AnyStr, source_id: int, destinations: List[AnyStr]
+) -> None:
     """Creates the files with selected server information and latencies"""
 
     dfs = pd.read_csv(datasets.servers)
-    dfs = dfs[dfs["country"].isin(DESTINATION_COUNTRIES)]
+    if not valid_id(dfs, source_id):
+        raise ValueError(f"Source id {source_id} is not valid")
+
+    for destination in destinations:
+        if not valid_country(dfs, destination):
+            raise ValueError(f"Destination {destination} is not valid")
+
+    dfs = dfs[dfs["country"].isin(destinations)]
     dfs.reset_index(inplace=True)
     dfs = dfs[["id", "title", "country", "latitude", "longitude"]]
+    dfs.rename(columns={"id": "destination"}, inplace=True)
 
     dfp = pd.read_csv(datasets.pings)
-    dfp = dfp[dfp["source"] == SOURCE_ID]
-    dfp = dfp[dfp["destination"].isin(dfs["id"])]
+    dfp = dfp[dfp["source"] == source_id]
+    dfp = dfp[dfp["destination"].isin(dfs["destination"])]
     dfp = dfp.groupby("destination").agg(
         min_latency=("min", lambda x: x.quantile(0.05)),
         max_latency=("max", lambda x: x.quantile(0.95)),
     )
     dfp.reset_index(inplace=True)
-    dfs.to_csv(path.join(output_dir, "locations.csv"), index=False)
-    dfp.round(decimals=4).to_csv(path.join(output_dir, "latencies.csv"), index=False)
+    dfl = pd.merge(dfs, dfp, on="destination")
+    dfl.drop("destination", axis=1, inplace=True)
+    latency_file = path.join(output_dir, "latencies.csv")
+    dfl.round(decimals=4).to_csv(latency_file, index=False)
+    print(f"Latency information saved as {latency_file}")
 
 
 @click.command()
 @click.option(
     "--output_dir", help="Directory where to store the produced data", default="./data"
 )
+@click.option(
+    "--source_id",
+    type=int,
+    help="The source from which latencies will be computed",
+    default=SOURCE_ID,
+)
+@click.option(
+    "--destination",
+    multiple=True,
+    help="Select the locations of the provided country",
+    default=DESTINATION_COUNTRIES,
+)
 def main(**options):
     """Entry point for executing this module."""
     output_dir = options["output_dir"]
+    source_id = options["source_id"]
+    destinations = options["destination"]
     cache_path = path.join(output_dir, CACHE_SUBDIR)
     makedirs(cache_path, exist_ok=True)
     datasets = fetch_data(cache_path=cache_path)
-    create_latency_files(datasets, output_dir)
+    try:
+        create_latency_files(datasets, output_dir, source_id, destinations)
+    except ValueError as e:
+        print(str(e))
